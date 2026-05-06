a443b421def94e80e18c5e6561f1ed7d1c1126a3
bwirem <bwirem11@gmail.com>
2026-05-06 07:39:46 +0300

    Updated various files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'GreyCare') }}</title>
 
         <!-- Favicon -->
         <link rel="icon" href="/img/favicon.ico?v=1.1" type="image/x-icon">
